491dcd01c724e89898a5dd727d2537faf8b76b7c
Sandro Vieira <sandro.vieira@solera.com>
2025-11-23 16:44:42 +0100

    Normalizing Application class name

--- src/main/java/com/slzvieira/study/webflux/Application.java
+++ src/main/java/com/slzvieira/study/webflux/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SlzvieiraWebFluxApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SlzvieiraWebFluxApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
