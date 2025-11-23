8c234a64b068fca33929b0ee7512f5bba70597c8
Sandro Vieira <sandro.vieira@solera.com>
2025-11-23 16:00:38 +0100

    Empty project generated by Spring Initializr (https://start.spring.io/)

---
+++ src/main/java/com/slzvieira/study/webflux/SlzvieiraWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.slzvieira.study.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlzvieiraWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlzvieiraWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slzvieira/study/webflux/SlzvieiraWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slzvieira.study.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlzvieiraWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
